1f699c65ed35dcddb02171c477e0b8e1c3859b3f
Wither <izawananakowo@gmail.com>
2024-11-07 02:21:57 +0900

    Remove dist folder from Git index

--- dist/commands/fun/owo.js
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-export const data = new SlashCommandBuilder()
-    .setName('owo')
-    .setDescription('owo!');
-export async function execute(interaction) {
-    await interaction.reply('owo!');
-}
--- dist/models/user.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../utils/database.js';
-const User = sequelize.define('user', {
-    id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-    }
-});
-export default User;
