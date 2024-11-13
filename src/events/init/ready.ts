3523ccdf93cc84d33c4ef5e701bf01bcfc084f16
Wither <izawananakowo@gmail.com>
2024-11-13 23:22:03 +0900

    Potential issue dealt with
    Fixed potential issue that if bot goes down, scheduled unban would be canceled.

--- src/events/init/ready.ts
+++ src/events/init/ready.ts
@@ -1,5 +1,10 @@
-import { Events, Client, PresenceStatusData, ActivityType } from 'discord.js';
+import { Events, Client, PresenceStatusData, ActivityType, EmbedBuilder } from 'discord.js';
+import { schedule } from 'node-cron';
 import BotSettings from '../../models/botSettings.js';
+import BannedMember from '../../models/bannedMember.js';
+import Guild from '../../models/guild.js';
+import sendLog from '../../utils/sendLog.js';
+import i18next from 'i18next';
 
 export const name = Events.ClientReady;
 export const once = true;
@@ -77,6 +82,88 @@ export async function execute(client: Client) {
                 type: activityType,
             }],
             status: status,
+        });
+    }
+
+    const bannedMembers = await BannedMember.findAll({
+        where: {
+            isBanned: true,
+        }
+    });
+
+    for (const bannedMember of bannedMembers) {
+        if (!bannedMember.bannedUntil) {
+            continue;
+        }
+
+        const guild = await client.guilds.fetch(bannedMember.guildId);
+        const memberFetched = await guild.members.fetch(bannedMember.id);
+        const guildData = await Guild.findOne({
+            where: {
+                id: guild.id,
+            }
+        });
+        const clientUser = await guild.members.fetch(client.user.id);
+
+        i18next.changeLanguage(guildData?.language);
+        const unbanEmbedTitle = i18next.t('ban.unbanEmbedTitle');
+        const unbanEmbedFooter = i18next.t('ban.unbanEmbedFooter');
+        const userLiteral = i18next.t('ban.userLiteral');
+        const reasonLiteral = i18next.t('ban.reasonLiteral');
+        const banExpiredMessage = i18next.t('ban.banExpiredMessage');
+
+        async function unban() {
+            const unbanEmbed = new EmbedBuilder()
+            .setColor('#FF0000')
+            .setTitle(unbanEmbedTitle)
+            .addFields([
+                {
+                    name: userLiteral,
+                    value: `${memberFetched}`,
+                    inline: true,
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true,
+                },
+                {
+                    name: reasonLiteral,
+                    value: banExpiredMessage,
+                    inline: true,
+                },
+            ])
+            .setTimestamp()
+            .setFooter({
+                text: unbanEmbedFooter,
+                iconURL: clientUser.avatarURL() ?? undefined,
+            });
+    
+            await guild.members.unban(memberFetched);
+            await sendLog(guild, {
+                embeds: [unbanEmbed],
+            });
+        }
+
+        if (bannedMember.bannedUntil.getTime() < Date.now()) {
+            await unban();
+            await bannedMember.update({
+                isBanned: false,
+                bannedUntil: null,
+            });
+            continue;
+        }
+
+        schedule(`${bannedMember.bannedUntil}`, async () => {
+            if (bannedMember.isBanned === false) {
+                return;
+            }
+
+            await unban();
+            await bannedMember.update({
+                isBanned: false,
+                bannedUntil: null,
+            });
         });
     }
 }
