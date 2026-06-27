62fab66db9d066c3cb4135b7fd3b0dd70dea0709
Saduwub <143506996+IzawaNanako@users.noreply.github.com>
2026-06-28 00:08:56 +0900

    Made database credentials more easily configurable

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,11 @@
-import { Sequelize } from 'sequelize';
+import { Dialect, Sequelize } from 'sequelize';
 
 const database = process.env.DATABASE;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
+const dialect = process.env.DB_DIALECT as Dialect || 'postgres' as Dialect;
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
 
 if (!database || !user || !password) {
     console.error('Database credentials not found.');
@@ -13,8 +16,8 @@ if (!database || !user || !password) {
  * The database connection.
  */
 export const sequelize = new Sequelize(database, user, password, {
-    dialect: 'postgres',
-    host: 'nanazdb', 
-    port: 5432,
+    dialect,
+    host,
+    port,
     logging: false,
 });
